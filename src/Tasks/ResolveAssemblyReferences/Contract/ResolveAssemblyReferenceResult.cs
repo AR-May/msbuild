62ee82aa9ed9fbdde021af03745fdcaf7de17fbf
AR-May <67507805+AR-May@users.noreply.github.com>
2021-02-08 15:24:45 +0100

    rar communication : add contract classes drafts

---
+++ src/Tasks/ResolveAssemblyReferences/Contract/ResolveAssemblyReferenceResult.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using Microsoft.Build.Framework;
+
+namespace Microsoft.Build.Tasks.ResolveAssemblyReferences.Contract
+{
+    internal sealed class ResolveAssemblyReferenceResult
+    {
+        public ResolveAssemblyReferenceResult()
+        {
+        }
+
+        internal ResolveAssemblyReferenceResult(bool taskResult, ResolveAssemblyReferenceResponse response)
+        {
+            TaskResult = taskResult;
+            Response = response;
+        }
+
+        public bool TaskResult { get; set; }
+
+        public ResolveAssemblyReferenceResponse Response { get; set; }
+
+        public List<BuildEventArgs> BuildEvents { get; set; }
+    }
+}
